473654f8ed9d00a847bc77a72974450722a115a9
Bijay <bijay78082@gmail.com>
2025-12-20 12:32:00 +0530

    terraform

--- roboshop-v1/roboshop.tf
+++ roboshop-v1/roboshop.tf
@@ -26,6 +26,14 @@ resource "aws_instance" "mongodb" {
   }
 }
 
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z02655483UAEAXVIBG9GU"
+  name    = "mongodb-dev.learntechnology.space"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
+}
+
 resource "aws_instance" "catalogue" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
@@ -34,6 +42,14 @@ resource "aws_instance" "catalogue" {
   tags = {
     Name = "catalogue"
   }
+}
+
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z02655483UAEAXVIBG9GU"
+  name    = "catalogue-dev.learntechnology.space"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.catalogue.private_ip]
 }
 
 resource "aws_instance" "cart" {
@@ -46,6 +62,15 @@ resource "aws_instance" "cart" {
   }
 }
 
+resource "aws_route53_record" "cart" {
+  zone_id = "Z02655483UAEAXVIBG9GU"
+  name    = "cart-dev.learntechnology.space"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.cart.private_ip]
+}
+
+
 resource "aws_instance" "redis" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
@@ -54,6 +79,71 @@ resource "aws_instance" "redis" {
   tags = {
     Name = "redis"
   }
+}
+
+resource "aws_route53_record" "redis" {
+  zone_id = "Z02655483UAEAXVIBG9GU"
+  name    = "redis-dev.learntechnology.space"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.redis.private_ip]
+}
+
+
+
+resource "aws_instance" "mysql" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-03df7f4a9485bc781"]
+
+  tags = {
+    Name = "mysql"
+  }
+}
+
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z02655483UAEAXVIBG9GU"
+  name    = "mysql-dev.learntechnology.space"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
+}
+
+
+resource "aws_instance" "shipping" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-03df7f4a9485bc781"]
+
+  tags = {
+    Name = "shipping"
+  }
+}
+
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z02655483UAEAXVIBG9GU"
+  name    = "shipping-dev.learntechnology.space"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.shipping.private_ip]
+}
+
+resource "aws_instance" "rabbitmq" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-03df7f4a9485bc781"]
+
+  tags = {
+    Name = "rabbitmq"
+  }
+}
+
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z02655483UAEAXVIBG9GU"
+  name    = "rabbitmq-dev.learntechnology.space"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.rabbitmq.private_ip]
 }
 
 resource "aws_instance" "user" {
@@ -66,32 +156,10 @@ resource "aws_instance" "user" {
   }
 }
 
-resource "aws_instance" "mysql" {
-  ami           = "ami-09c813fb71547fc4f"
-  instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-03df7f4a9485bc781"]
-
-  tags = {
-    Name = "mysql"
-  }
-}
-
-resource "aws_instance" "shipping" {
-  ami           = "ami-09c813fb71547fc4f"
-  instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-03df7f4a9485bc781"]
-
-  tags = {
-    Name = "shipping"
-  }
-}
-
-resource "aws_instance" "rabbitmq" {
-  ami           = "ami-09c813fb71547fc4f"
-  instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-03df7f4a9485bc781"]
-
-  tags = {
-    Name = "rabbitmq"
-  }
+resource "aws_route53_record" "user" {
+  zone_id = "Z02655483UAEAXVIBG9GU"
+  name    = "user-dev.learntechnology.space"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.user.private_ip]
 }
