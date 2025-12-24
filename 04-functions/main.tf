96506c024d6f8dcf147f0bebf43be93283250294
Bijay <bijay78082@gmail.com>
2025-12-24 12:25:23 +0530

    terraform

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -6,3 +6,13 @@ output "fruit" {
  // value = var.fruits[2]
   value = element(var.fruits, 3)
 }
+
+variable "fruits_stock" {
+  default = {
+    apple = 100
+  }
+}
+
+output "fruit_stock" {
+  value = var.fruits_stock["banana"]
+}
