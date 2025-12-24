ca14e071125159d40093b65e69270a8266038444
Bijay <bijay78082@gmail.com>
2025-12-24 12:40:43 +0530

    terraform

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -7,12 +7,18 @@ output "fruit" {
   value = element(var.fruits, 3)
 }
 
-variable "fruits_stock" {
+variable "fruits_with_stock" {
   default = {
-    apple = 100
+    apple =
+    stock = 100
   }
 }
 
 output "fruit_stock" {
-  value = try(var.fruits_stock["banana"], 0)
+  value = try(var.fruits_with_stock["banana"], 0)
+}
+
+output "fruit_stock_price" {
+  value = lookup(var.fruits_with_stock["apple"], "price", 2)
+           //try(var.fruits_with_stock["apple"]["price"], 2)
 }
