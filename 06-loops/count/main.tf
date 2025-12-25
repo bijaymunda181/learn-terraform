4d4a2e5d27698f8a68a55947223512ad0dcaa5a5
Bijay <bijay78082@gmail.com>
2025-12-25 10:20:19 +0530

    terraform

---
+++ 06-loops/count/main.tf
@@ -0,0 +1,15 @@
+variable "components" {
+  default = ["frontend", "mongodb"]
+}
+
+resource "aws_instance" "component" {
+  count = length(var.components)
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-03df7f4a9485bc781"]
+
+  tags = {
+    //Name = var.component[count.index]
+    Name = element(var.components,count.index)
+  }
+}
