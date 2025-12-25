2364b0c7bbf3304b7c7cb4b130f6fdca94e7724c
Bijay <bijay78082@gmail.com>
2025-12-25 10:57:11 +0530

    terraform

--- 06-loops/count/main.tf
+++ 06-loops/count/main.tf
@@ -1,15 +1,22 @@
 variable "components" {
-  default = ["catalogue", "frontend", "mongodb", ]
+  default = [frontend", "mongodb", ]
 }
 
-resource "aws_instance" "component" {
+# resource "aws_instance" "component" {
+#   count = length(var.components)
+#   ami           = "ami-09c813fb71547fc4f"
+#   instance_type = "t3.small"
+#   vpc_security_group_ids = ["sg-03df7f4a9485bc781"]
+#
+#   tags = {
+#     //Name = var.component[count.index]
+#     Name = element(var.components,count.index)
+#   }
+# }
+
+resource "aws_security_group" "allow_tls" {
   count = length(var.components)
-  ami           = "ami-09c813fb71547fc4f"
-  instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-03df7f4a9485bc781"]
+  name        = element(var.components,count.index )
 
-  tags = {
-    //Name = var.component[count.index]
-    Name = element(var.components,count.index)
-  }
-}
+
+
