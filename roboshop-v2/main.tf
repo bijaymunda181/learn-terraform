74293733adfd9d04b3d39d43897417b689767225
Bijay <bijay78082@gmail.com>
2025-12-25 12:23:07 +0530

    terraform

--- roboshop-v2/main.tf
+++ roboshop-v2/main.tf
@@ -1,19 +1,51 @@
-//variable "components" {
- //default = [ "frontend", "mongodb", "catalogue", ]
-//}
+variable "ami" {
+  default = "ami-09c813fb71547fc4f"
+}
 
-//variable "component" {
-//  default = {
-//    frontend = {
-//     instance_type = "t3.macro"
-//    }
-//    mongodb = {
-//      instance_type = "t3.small"
-//    }
-//    catalogue = {
-//      instance_type = "t3.small"
-//    }
-//  }
-//}
+variable "instance_type" {
+  default = "t3.small"
+}
+
+variable "security_groups" {
+  default = ["sg-03df7f4a9485bc781"]
+}
+
+variable "zone_id" {
+  default = "Z02655483UAEAXVIBG9GU"
+}
+
+variable "components" {
+  default = {
+    frontend ={ name = "frontend" }
+    catalogue ={name = "catalogue" }
+    mongodb ={ name = "mongodb" }
+    user ={ name = "user" }
+    redis ={ name = "redis" }
+    cart ={ name = "cart" }
+    mysql ={ name = "mysql" }
+    shipping ={ name = "shipping" }
+    payment ={ name = "payment" }
+    rabbitmq ={ name = " rabbitmq" }
+  }
+}
 
 
+resource "aws_instance" "instance" {
+  for_each = var.components
+  ami           = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.security_groups
+
+  tags = {
+    Name = lookup(each.value, "name", null)
+  }
+}
+
+# resource "aws_route53_record" "record" {
+#
+#   zone_id = var.zone_id
+#   name    = "frontend-dev.learntechnology.space"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.frontend.private_ip]
+# }
