2bae9dfc5f26f106835553b92c03adedc19bec30
Bijay <bijay78082@gmail.com>
2026-01-06 21:54:56 +0530

    terraform

--- 05-data-sources/main.tf
+++ 05-data-sources/main.tf
@@ -1,10 +1,18 @@
 # https://www.youtube.com/watch?v=_NU3jIwvPGs&t=554s
 
-data "aws_ami_ids" "ami" {
-  name_regex       = "Centos-*"
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex       = "Centos-8-DevOps-Practice"
   owners           = ["973714476881"]
 }
 
 output "ami" {
-  value = data.aws_ami_ids.ami
+  value = data.aws_ami.ami
 }
+
+resource "aws_instance" "instance" {
+  ami           = data.aws_ami.ami
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0bdb031df53cf61e0"]
+}
+
